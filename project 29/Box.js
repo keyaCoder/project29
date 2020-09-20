d1af480d212742137ab419db848dddc695ff7c67
keyaCoder <65353210+keyaCoder@users.noreply.github.com>
2020-09-20 17:08:29 +0530

    Add files via upload

---
+++ project 29/Box.js
@@ -0,0 +1,18 @@
+class Box{
+  constructor(x, y, width, height){
+    var options = {
+      'isStatic' : false, 
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    this.radius = width/2;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    fill("purple");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+  }
